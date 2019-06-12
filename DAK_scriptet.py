7ecd2acd1aefe946816d251218b35ebc6b161ee6
Katofln <frederikstroem@pm.me>
2019-06-12 12:13:17 +0200

    Added possibility to read settings.json

--- DAK_scriptet.py
+++ DAK_scriptet.py
@@ -1,7 +1,12 @@
 import tkinter as tk
+import json
+import os
 
 class DAKScreen:
     def __init__(self):
+        self.settingsJSONPath = "./settings.json"
+        self.settings = self.getSettings()
+
         self.tk = tk.Tk()
         self.tk.title("DAK_scriptet")
         self.tk.attributes("-fullscreen", True)
@@ -13,6 +18,33 @@ class DAKScreen:
         self.canvas.config(bg="#FF0000")
 
         self.tk.mainloop()
+
+    def setStandardSettings(self):
+        # Standard settings.
+        settings = {
+            "FPS": 5,
+            "randomColors": False,
+            "colors": [
+                "#ff0000",
+                "#00ff00",
+                "#0000ff"
+            ]
+        }
+        # Write to file.
+        with open(self.settingsJSONPath, "w") as f:
+            f.write(json.dumps(settings, indent=2))
+
+    def getSettings(self, asString=False):
+        settings = None
+
+        if os.path.isfile(self.settingsJSONPath) is False:
+            self.setStandardSettings()
+
+        # Read file.
+        with open(self.settingsJSONPath, "r") as f:
+            settings = json.loads(f.read())
+
+        return settings
 
     def quit(self, event=None):
         self.tk.destroy()
