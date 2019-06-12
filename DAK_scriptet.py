51d24d7707ef11bd2aff1f9930ba185ee2733179
Katofln <frederikstroem@pm.me>
2019-06-12 12:01:10 +0200

    Updated gitignore and general improvements

--- DAK_scriptet.py
+++ DAK_scriptet.py
@@ -6,9 +6,11 @@ class DAKScreen:
         self.tk.title("DAK_scriptet")
         self.tk.attributes("-fullscreen", True)
         self.tk.config(cursor="none")
-        self.canvas = tk.Canvas(self.tk, highlightthickness=0, bg="blue")
+        self.canvas = tk.Canvas(self.tk, highlightthickness=0)
         self.canvas.pack(fill=tk.BOTH, expand=1)
         self.tk.bind("<Escape>", self.quit)
+
+        self.canvas.config(bg="#FF0000")
 
     def quit(self, event=None):
         self.tk.destroy()
